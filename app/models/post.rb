06c8ee3a9694a07bd10ef99fab5ff7cb5fc28992
Pritchard45 <alexpritchard45@gmail.com>
2018-10-30 13:26:50 -0400

    assignment post user complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC') }
+  #for assignment
+  scope :ordered_by_title, -> { order('title DESC') }
+  # for assignment
+  scope :ordered_by_reverse_created_at, -> { reorder('created_at ASC') }
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: { minimum: 20}, presence: true
