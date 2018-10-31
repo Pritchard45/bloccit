6740bf9eb9e0d6fbf88a13470b66181890e6d840
Pritchard45 <alexpritchard45@gmail.com>
2018-10-31 00:34:16 -0400

    assignment 27 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,6 +32,9 @@ RSpec.describe User, type: :model do
      it "responds to member?" do
        expect(user).to respond_to(:member?)
      end
+
+     it "responds to moderator?" do
+       expect(user).to respond_to(:moderator?)
    end
 
    describe "roles" do
@@ -48,8 +51,12 @@ RSpec.describe User, type: :model do
        it "returns false for #admin?" do
          expect(user.admin?).to be_falsey
        end
+
+       it "returns false for #moderator?"
+        expect(user.moderator?).to be_falsey
+      end
      end
- 
+
      context "admin user" do
        before do
          user.admin!
@@ -62,8 +69,30 @@ RSpec.describe User, type: :model do
        it "returns true for #admin?" do
          expect(user.admin?).to be_truthy
        end
+
+       it "returns false for #moderator?"
+         expect(user.moderator?).to be_falsey
      end
    end
+
+   context "moderator user" do
+      before do
+          user.moderator!
+      end
+
+      it "returns false for #member?" do
+          expect(user.member?).to be_falsey
+      end
+
+      it "returns false for #admin?" do
+          expect(user.admin?).to be_falsey
+      end
+
+      it "returns true for #moderator?" do
+          expect(user.moderator?).to be_truthy
+      end
+    end
+  end
 
 
   describe "invalid user" do
