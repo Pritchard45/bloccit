9b6731f50f6966b01baef6c95058b3b1d220fcce
Pritchard45 <alexpritchard45@gmail.com>
2018-11-11 20:27:42 -0500

    added post callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,31 +18,40 @@ class Post < ApplicationRecord
   validates :topic, presence: true
   validates :user, presence: true
 
-     def up_votes
-     votes.where(value: 1).count
-   end
+  def up_votes
+    votes.where(value: 1).count
+  end
 
-   def down_votes
-     votes.where(value: -1).count
-   end
+  def down_votes
+    votes.where(value: -1).count
+  end
 
-   def points
-     votes.sum(:value)
-   end
+  def points
+    votes.sum(:value)
+  end
 
-   def update_rank
-     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
-     new_rank = points + age_in_days
-     update_attribute(:rank, new_rank)
-   end
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+    new_rank = points + age_in_days
+    update_attribute(:rank, new_rank)
+  end
 
-   def create_vote
-     user.votes.create(value: 1, post: self)
-   end
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+
+  private
+
+  def create_favorite
+    user.favorites.create(post: self)
+  end
+
+   after_create :send_favorite_emails
 
    private
 
-   def create_favorite
-     user.favorites.create(post: self)
+   def send_favorite_emails
+     post.favorites.each do |favorite|
+       FavoriteMailer.new_post(favorite.user, post, self).deliver_now
    end
 end
